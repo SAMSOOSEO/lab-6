ac5fad3047fac240918d24904e9a8dd57267f6ee
SAMSOOSEO <samsooseo@hotmail.com>
2025-11-04 20:02:19 +0800

    d
    d

--- meta/main.js
+++ meta/main.js
@@ -4,17 +4,18 @@ import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
 
 async function loadData() {
-const data = await d3.csv(`loc.csv?t=${Date.now()}`, (row) => ({
-    ...row,
-    line: Number(row.line),
-    depth: Number(row.depth),
-    length: Number(row.length),
-    date: new Date(row.date + 'T00:00' + (row.timezone || '+09:00')),
-    datetime: new Date(row.datetime),
-}));
+   const data = await d3.csv('loc.csv', (row) => ({
+        ...row,
+        line: Number(row.line),
+        depth: Number(row.depth),
+        length: Number(row.length),
+        date: new Date(row.date + 'T00:00' + (row.timezone || '+09:00')),
+        datetime: new Date(row.datetime),
+    }));
 
     return data;
 }
+
 
 
 
